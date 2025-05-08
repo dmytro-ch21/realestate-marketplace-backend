8dd591cb31e003f2a208d53e4740a823148b18af
Dmytro Chobotar <dchobotar.fl@gmail.com>
2025-05-07 22:14:08 -0400

    implemented auth controller and service layer

---
+++ app/controllers/auth_controller.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, request, jsonify
+from app.utils.validators import is_valid_email, is_valid_password, is_valid_username
+from app.services.auth_service import AuthService
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    """Register new user
+    Request body:
+    {
+        "email": "",
+	    "username": "new_username",
+	    "password": "qwerty" 
+    }
+    Returns 
+        201: User successfully created
+        400: Validation error
+    """
+    data = request.get_json()
+    
+    # Manual validation
+    if "email" not in data or not data.get("email"):
+        return jsonify({"error": "email property cannot be empty or missing"}), 400
+    
+    if not is_valid_email(data.get("email")):
+        return jsonify({"error": f"Invalid email format {data.get("email")}"}), 400
+    
+    if "username" not in data or not data.get("username"):
+        return jsonify({"error": "username property cannot be empty or missing"}), 400
+    
+    if not is_valid_username(data.get("username")):
+        return jsonify({"error": f"Invalid username format cannot contain spaces '{data.get("username")}'"}), 400
+    
+    if "password" not in data or not data.get("password"):
+        return jsonify({"error": "password property cannot be empty or missing"}), 400
+    
+    if not is_valid_password(data.get("password")):
+        return jsonify({"error": f"Invalid password format {data.get("password")}"}), 400
+    
+    try:
+        user = AuthService.register(**data)
+        return jsonify(
+            {
+                "message": "User successfully created!", 
+                "user": user.serialize(),
+            }
+        )
+    except ValueError as e:
+        return jsonify({"error": "Registration failed", "message": str(e)}), 400    
+    
+
+
+  
---
+++ app/services/auth_service.py
@@ -0,0 +1,27 @@
+from app.repositories.user_repository import UserRepository
+from app.repositories.profile_repository import ProfileRepository
+from app.models.user import User
+from typing import Optional
+
+
+class AuthService:
+    @staticmethod
+    def register(email: str, username: str, password: str) -> User:
+        # implement business rules here
+        # a user cannot register with already existing email
+        # a user cannot register with already existing username
+        
+        if UserRepository.get_by_email(email):
+            raise ValueError(f"Email '{email}' already exists. Please try another one.")
+        
+        if UserRepository.get_by_username(username):
+            raise ValueError(f"Username '{username}' already exists. Please try another one.")
+        
+        user = UserRepository.create_user(email, username, password)
+        
+        # create a profile for the user along with user creation
+        ProfileRepository.create(user.id)
+        
+        return user
+        
+        
